36c16d6ed658ae0203bd3e424a5d265c97ab42f1
Pablo Matheus <pablomatheus171@gmail.com>
2022-12-03 21:05:54 -0400

    chore: config lint staged with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/return-await': 'off',
+    'prefer-const': 'off'
   }
 }
