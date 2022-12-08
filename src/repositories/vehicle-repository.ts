d8a929e39d5e1d4f0083b06fdb67357e2bbf89a0
Pablo Matheus <pablomatheus171@gmail.com>
2022-12-08 02:01:09 -0400

    refactor: remove Vehicle creation from Vehicle Repository

--- src/repositories/vehicle-repository.ts
+++ src/repositories/vehicle-repository.ts
@@ -1,13 +1,11 @@
 import Repository from '../domain/interfaces/repository-interface'
 import Database from '../domain/interfaces/database-interface'
 import Vehicle from '../domain/entities/vehicle'
-import VehicleDTO from '../input/dtos/vehicle-DTO'
 
 export default class VehicleRepository implements Repository {
   constructor (private readonly database: Database) { }
 
-  async create ({ name, driver, model, licensePlate, type }: VehicleDTO): Promise<any> {
-    const vehicle = new Vehicle(driver, name, model, licensePlate, type)
+  async create (vehicle: Vehicle): Promise<any> {
     const saveVehicle = await this.database.save(vehicle)
     if (saveVehicle) {
       return saveVehicle
