4ef4ae5c68c7ccd702cde43d13b5177e992ace02
Pablo Matheus <pablomatheus171@gmail.com>
2022-12-11 20:43:14 -0400

    chore: add handlebars and jsbarcode

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,9 +1,15 @@
 import bodyParser from 'body-parser'
+import path from 'path'
 import express from 'express'
+import { engine } from 'express-handlebars'
 import { startRoute } from '../routes/start.route'
+// routes
 
 const app = express()
-
 app.use(bodyParser.json())
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname + '/' + '../views'))
+
 app.use(startRoute)
 export default app
--- src/app/express-route-adapter.ts
+++ src/app/express-route-adapter.ts
@@ -10,6 +10,10 @@ export default class ExpressRouteAdapter {
       }
 
       const serverResponse = await controller.handle(clientRequest)
+      if (serverResponse.status === 201) {
+        const response = serverResponse.response
+        return res.render('codebar', { response })
+      }
       res.status(serverResponse.status)
         .json(serverResponse.response)
     }
