f7dbf1d1b7b39bed8d7c03e6f4da9dcd8c344ba7
Pablo Matheus <pablomatheus171@gmail.com>
2022-12-17 15:03:43 -0400

    refactor: create class to generate view and other class to generate barcode

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,15 +1,10 @@
 import bodyParser from 'body-parser'
-import path from 'path'
 import express from 'express'
-import { engine } from 'express-handlebars'
 import { startRoute } from '../routes/start.route'
 // routes
 
 const app = express()
 app.use(bodyParser.json())
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname + '/' + '../views'))
-app.use('/tmp', express.static(path.join(__dirname, '../tmp')))
+
 app.use(startRoute)
 export default app
--- src/app/express-route-adapter.ts
+++ src/app/express-route-adapter.ts
@@ -1,19 +1,17 @@
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable quote-props */
+import {
+  GenerateView,
+  GenerateBarCode
+} from './generate-ticket/generate-ticket'
 import { Request, Response } from 'express'
-import fs from 'fs'
-import path from 'path'
 import ClientRequest from '../input/helpers/client-request'
 import Controller from '../input/protocols/controller'
-import JsBarcode from 'jsbarcode'
-import { createCanvas } from 'canvas'
 
-let canvas = createCanvas(100, 100)
-
-JsBarcode(canvas, '12434348578')
-const buffer = canvas.toBuffer('image/png')
-// eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-const file = path.join(__dirname + '/tmp/' + 'codebar' + '.png')
-console.log(file)
-fs.writeFileSync(file, buffer)
+// const template = Handlebars.compile(source)
 
 export default class ExpressRouteAdapter {
   static execute (controller: Controller) {
@@ -23,30 +21,31 @@ export default class ExpressRouteAdapter {
       }
 
       const serverResponse = await controller.handle(clientRequest)
+      const { ticket, licensePlate, created_at, type } =
+        serverResponse.response
       if (serverResponse.status === 201) {
-        const response = serverResponse.response
-
-        return res.status(201).render('codebar', { response })
+        const source =
+          '<p>ticket:{{ticket}}</p>' +
+          '<br>' +
+          '<p>licensePlate:{{licensePlate}}</p>' +
+          '<br>' +
+          '<p>Hour:{{created_at}}</p>' +
+          '<br>' +
+          '<p>type:{{type}}</p>' +
+          '<br>' +
+          '<img src="{{img}}">'
+        const data = {
+          'ticket': `${ticket}`,
+          'licensePlate': `${licensePlate}`,
+          'hour': `${created_at}`,
+          'type': `${type}`,
+          'img': `../tmp/codebar${ticket}.png`
+        }
+        await new GenerateView().generate(source, data)
+        await new GenerateBarCode().generate(ticket)
+        return res.status(serverResponse.status).json(serverResponse.response)
       }
-      res.status(serverResponse.status)
-        .json(serverResponse.response)
+      res.status(serverResponse.status).json(serverResponse.response)
     }
   }
 }
-
-// export default class RouteAdapter {
-//   controller: Controller
-//   constructor (controller: Controller) {
-//     this.controller = controller
-//   }
-
-//   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-//   async handle (request: Request, response: Response): Promise<Response> {
-//     const clientRequest: ClientRequest = {
-//       request: request.body
-//     }
-//     const serverResponse = await this.controller.handle(clientRequest)
-//     return response.status(serverResponse.status)
-//       .json(serverResponse.response)
-//   }
-// }
