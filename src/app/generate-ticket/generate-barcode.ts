5f1dcdc0583c602e66b340ee90efb92578c05a28
Pablo Matheus <pablomatheus171@gmail.com>
2022-12-20 19:01:34 -0400

    refactor(GenerateBarCode): add display value equal false

--- src/app/generate-ticket/generate-barcode.ts
+++ src/app/generate-ticket/generate-barcode.ts
@@ -6,7 +6,7 @@ import path from 'path'
 export class GenerateBarCode {
   async generate (value: string): Promise<void> {
     const canvas = createCanvas(100, 100)
-    JsBarcode(canvas, value)
+    JsBarcode(canvas, value, { displayValue: false })
     const buffer = canvas.toBuffer('image/png')
     await fs.writeFile(path.join(__dirname + '/codebar/' + `codebar${value}` + '.png'), buffer)
   }
--- src/app/generate-ticket/helpers/template.ts
+++ src/app/generate-ticket/helpers/template.ts
@@ -1,14 +1,20 @@
 /* eslint-disable quote-props */
 export const source =
-'<p>ticket:{{ticket}}</p>' +
-'<br>' +
-'<p>licensePlate:{{licensePlate}}</p>' +
-'<br>' +
-'<p>Hour:{{created_at}}</p>' +
-'<br>' +
-'<p>type:{{type}}</p>' +
-'<br>' +
-'<img src="{{img}}">'
+'<style>' +
+'*{margin: 0;padding: 0;}' +
+'.container{ display:flex; width: 13rem; height:15rem; padding:1rem; margin-left:10px; flex-direction:column; font-family: Arial, Helvetica, sans-serif; border: 1px solid #000;}' +
+'p{ margin: 10px; font-size: .7rem; margin-left: 3px;}' +
+'span{justify-content: center;margin: auto;}' +
+'</style>' +
+'<div class="container"><p><strong>ticket:{{ticket}}</strong></p>' +
+
+'<p><strong>licensePlate:{{licensePlate}}</strong></p>' +
+
+'<p><img src="{{img}}"></p>' +
+
+'<p><strong>type:{{type}}</strong></p>' +
+
+'<p><strong>Hour:{{created_at}}</strong></p></div>'
 
 export function content (
   ticket: string,
