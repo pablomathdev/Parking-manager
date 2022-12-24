86c3f90558516d47c9c46cb018e6eb07ac259606
Pablo Matheus <pablomatheus171@gmail.com>
2022-12-24 19:39:00 -0400

    refactor: remove all views after all tests

--- src/app/routes/start.route.spec.ts
+++ src/app/routes/start.route.spec.ts
@@ -1,11 +1,20 @@
-
+import fs from 'fs/promises'
+import path from 'path'
 import request from 'supertest'
 import DatabaseJson from '../../data/db/database-json'
 import app from '../config/app'
+
 describe('start parking', () => {
   afterEach(async () => {
     await new DatabaseJson('vehicle').clear()
     await new DatabaseJson('ticket').clear()
+  })
+  afterAll(async () => {
+    const directory = path.join(__dirname, '../generate-ticket/view/')
+
+    for (const file of await fs.readdir(directory)) {
+      await fs.unlink(path.join(directory, file))
+    }
   })
   test('should returns 201 if vehicle added', async () => {
     await request(app).post('/start').send({
