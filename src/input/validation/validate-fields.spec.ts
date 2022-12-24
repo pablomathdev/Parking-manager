9165aad82903d0684a94722af96020577e2363ac
Pablo Matheus <pablomatheus171@gmail.com>
2022-12-24 14:56:36 -0400

    refactor: edited request

--- src/input/validation/validate-fields.spec.ts
+++ src/input/validation/validate-fields.spec.ts
@@ -1,12 +1,11 @@
 import ValidateFields from './validate-fields'
 
 describe('Validate Fields', () => {
-  test('should return an error if name is not provided', () => {
+  test('should return an error if email is not provided', () => {
     const sut = new ValidateFields()
 
     const request = {
-      driver: 'any_driver',
-      model: 'any_model',
+
       licensePlate: 'XXXXX',
       type: 'any_type'
     }
@@ -14,7 +13,7 @@ describe('Validate Fields', () => {
     const result = sut.validate(request)
     expect(result).toEqual(new Error('fill in all fields !'))
   })
-  test('should return an error if name is not provided', () => {
+  test('should return an error if no field is provided', () => {
     const sut = new ValidateFields()
 
     const request = {}
