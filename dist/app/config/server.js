31bd1b8974c4eb71782c005de1b07c8eee98f142
Pablo Matheus <pablomatheus171@gmail.com>
2023-01-08 23:28:12 -0400

    refactor: remove folder dist

--- dist/app/config/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-require("./dotenv");
-var _app = _interopRequireDefault(require("./app"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-_app.default.listen(process.env.PORT, () => {
-  console.log(`running on port ${process.env.PORT}`);
-});
--- dist/mail/helpers/generate-barcode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.generateBarcode = generateBarcode;
-function generateBarcode(value) {
-  return `https://barcode.tec-it.com/barcode.ashx?data=${value}&code=Code25IL&eclevel=L`;
-}
