b7e91b9a8f4bdf603578083667a0dd7567afdbd8
Pablo Matheus <pablomatheus171@gmail.com>
2023-01-13 10:16:38 -0400

    refactor: moved register vehicle use case to folder

--- src/use-cases/register-vehicle/register-vehicle-usecase.ts
+++ src/use-cases/register-vehicle/register-vehicle-usecase.ts
@@ -1,9 +1,9 @@
-import UseCase from '../input/protocols/use-case'
-import VehicleRepositoryInterface from '../domain/interfaces/vehicle-repository-interface'
-import VehicleDTO from '../input/dtos/vehicle-DTO'
-import Repository from '../domain/interfaces/repository-interface'
-import Ticket from '../domain/entities/ticket'
-import Vehicle from '../domain/entities/vehicle'
+import UseCase from '../../input/protocols/use-case'
+import VehicleRepositoryInterface from '../../domain/interfaces/vehicle-repository-interface'
+import VehicleDTO from '../../input/dtos/vehicle-DTO'
+import Repository from '../../domain/interfaces/repository-interface'
+import Ticket from '../../domain/entities/ticket'
+import Vehicle from '../../domain/entities/vehicle'
 
 export default class RegisterVehicleUseCase implements UseCase {
   constructor (private readonly vehicleRepository: VehicleRepositoryInterface,
