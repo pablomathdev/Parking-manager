e30ec0136eef5b648411e56be444cd39f65e4d26
Pablo Matheus <pablomatheus171@gmail.com>
2023-01-13 10:07:34 -0400

    test: make sure StopParkingController returns 404 if use case not returns a vehicle

--- src/input/controllers/stop-parking/stop-parking-controller.spec.ts
+++ src/input/controllers/stop-parking/stop-parking-controller.spec.ts
@@ -6,10 +6,26 @@ import UseCase from 'input/protocols/use-case'
 class StopParkingController implements Controller {
   constructor (private readonly stopParkingUseCase: UseCase) {}
   async handle (clientRequest: ClientRequest): Promise<ServerResponse> {
-    const vehicle = await this.stopParkingUseCase.execute(clientRequest.request)
-    return {
-      status: 200,
-      response: vehicle
+    try {
+      const vehicle = await this.stopParkingUseCase.execute(
+        clientRequest.request
+      )
+
+      if (vehicle) {
+        return {
+          status: 200,
+          response: vehicle
+        }
+      }
+      return {
+        status: 404,
+        response: 'Item Not Exists!'
+      }
+    } catch {
+      return {
+        status: 500,
+        response: 'Internal Error'
+      }
     }
   }
 }
@@ -17,17 +33,17 @@ class StopParkingController implements Controller {
 const makeStopParkingUseCaseStub = (): any => {
   class StopParkingUseCaseStub implements UseCase {
     async execute (input: any): Promise<any> {
-      return new Promise(resolve => resolve({
-
-        id: 'fd4108f7-61fb-4b40-afd0-02318fb5a6ae',
-        id_vehicle: 'f41c53bc-b39c-4263-9330-7e11362fa16a',
-        type: 'any-type',
-        licensePlate: 'XXXXX',
-        ticket: '0123456789',
-        created_at: '01/12/2023',
-        hour: '8:22:05 PM'
-
-      }))
+      return new Promise((resolve) =>
+        resolve({
+          id: 'fd4108f7-61fb-4b40-afd0-02318fb5a6ae',
+          id_vehicle: 'f41c53bc-b39c-4263-9330-7e11362fa16a',
+          type: 'any-type',
+          licensePlate: 'XXXXX',
+          ticket: '0123456789',
+          created_at: '01/12/2023',
+          hour: '8:22:05 PM'
+        })
+      )
     }
   }
   return new StopParkingUseCaseStub()
@@ -58,7 +74,6 @@ describe('Stop parking controller', () => {
     const serverResponse = await sut.handle(clientRequest)
     expect(serverResponse.status).toBe(200)
     expect(serverResponse.response).toEqual({
-
       id: 'fd4108f7-61fb-4b40-afd0-02318fb5a6ae',
       id_vehicle: 'f41c53bc-b39c-4263-9330-7e11362fa16a',
       type: 'any-type',
@@ -66,7 +81,17 @@ describe('Stop parking controller', () => {
       ticket: '0123456789',
       created_at: '01/12/2023',
       hour: '8:22:05 PM'
-
     })
+  })
+  test('returns 404 if use case not returns a vehicle', async () => {
+    const { sut, stopParkingUseCaseStub } = systemUnderTest()
+    jest.spyOn(stopParkingUseCaseStub, 'execute')
+      .mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const clientRequest = {
+      request: '0123456789'
+    }
+    const serverResponse = await sut.handle(clientRequest)
+    expect(serverResponse.status).toBe(404)
+    expect(serverResponse.response).toEqual('Item Not Exists!')
   })
 })
