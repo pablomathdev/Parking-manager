204112e88fda650c5214016b17baf0487ee37ef4
Pablo Matheus <pablomatheus171@gmail.com>
2023-01-12 20:03:58 -0400

    refactor: add cors middleware

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,9 +1,15 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import { startRoute } from '../routes/start.route'
+import cors from 'cors'
 
 const app = express()
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  app.use(cors())
+  next()
+})
 app.use(bodyParser.json())
 app.use(startRoute)
 export default app
