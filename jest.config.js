72c339d82a3c3689ae537c51f7f009e7cdbffc8b
Pablo Matheus <pablomatheus171@gmail.com>
2023-01-18 00:01:19 -0400

    refactor: remove some files of coverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
 
 module.exports = {
+  roots: ['<rootDir>/src'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/providers/**',
+    '!<rootDir>/src/app/config/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/input/dtos/**',
+    '!<rootDir>/src/input/protocols/**',
+    '!<rootDir>/src/input/helpers/**'
+  ],
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
