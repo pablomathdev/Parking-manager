fca07fc079527869f948513bb44287c82e781b77
Pablo Matheus <pablomatheus171@gmail.com>
2023-01-25 12:29:24 -0400

    test: if you exceed the 15 minutes of tolerance, you must return the value of one hour

--- src/app/calc-price-per-hour.spec.ts
+++ src/app/calc-price-per-hour.spec.ts
@@ -26,6 +26,12 @@ function calcPricePerHour (typeVehicle: string, end_date: string): number {
 }
 
 describe('Calculate price per hour of parking', () => {
+  test('if you exceed the 15 minutes of tolerance, you must return the value of one hour', () => {
+    const resultMotocycle = calcPricePerHour('Motocycle', 'Wed, Jan 25, 2023 10:34 AM')
+    const resultCar = calcPricePerHour('Car', 'Wed, Jan 25, 2023 10:34 AM')
+    expect(resultMotocycle).toBe(2)
+    expect(resultCar).toBe(3)
+  })
   test('should return 0 if vehicle type is Motorcycle and still within the 15 minute tolerance', () => {
     const result = calcPricePerHour('Motocycle', 'Wed, Jan 25, 2023 10:35 AM')
     expect(result).toBe(0)
